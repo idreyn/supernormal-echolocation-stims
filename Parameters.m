809406fe71a60987f6382c93a0194767911f8015
Ian Reynolds <idreyn@gmail.com>
2020-11-01 19:36:53 -0600

    put some guardrails on sensor headings

--- Parameters.m
+++ Parameters.m
@@ -84,7 +84,7 @@ classdef Parameters
             c_max = medium.sound_speed_max;
             c_ref = medium.sound_speed_ref;
             dt_limit = 2 / (c_ref * k_max) * asin(c_ref / c_max);
-            dt = 0.5 * dt_limit
+            dt = 0.5 * dt_limit;
             params.grid.setTime(round(params.t_end_s / dt), dt);
         end
     end
--- hrir.m
+++ hrir.m
@@ -7,7 +7,7 @@ air = Material(343, 1.225);
 water = Material(1480, 1000);
 plastic = Material(2170, 1070);
 
-params = Parameters(5e4, 2 * impulse_radius_m + 0.01, 0.1);
+params = Parameters(5e4, 2 * impulse_radius_m + 0.01, 0.0001);
 medium = Medium(params, air);
 
 head_radius =  m_to_grid_points(params, head_radius_m);
@@ -17,9 +17,9 @@ apply_mask(medium, water, head);
 headband = make_headband(params, head_radius_m, headband_thickness_m);
 apply_mask(medium, plastic, headband);
 
-sensor_mask = make_sensor_mask(params, head_radius_m + headband_thickness_m + 0.001, 0:1:360);
+sensor_mask = make_sensor_mask(params, head_radius_m + headband_thickness_m + 0.001, -90:1:90);
 sensor = make_sensor(sensor_mask);
 source = make_impulse_source(params, impulse_radius_m, 0, 1e3);
 
 set_timestep_from_medium(params, medium);
-sensor_data = kspaceFirstOrder2D(params.grid, get_struct(medium), source, sensor, 'SaveToDisk', 'hrir.h5');
+sensor_data = kspaceFirstOrder2D(params.grid, get_struct(medium), source, sensor);
